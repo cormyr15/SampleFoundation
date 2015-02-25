c2ed00da14de1fb2270ccf823925928779bfa63f
Jei <htago@isbx.com>
2015-02-25 18:54:18 +0800

    Initial Commit of Library
    Signed-off-by: Jei <htago@isbx.com>

---
+++ SampleFoundation/SampleFoundation.h
@@ -0,0 +1,13 @@
+//
+//  SampleFoundation.h
+//  SampleFoundation
+//
+//  Created by Harold James D. Tago on 2/25/15.
+//  Copyright (c) 2015 Harold James D. Tago. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleFoundation : NSObject
+
+@end
---
+++ SampleFoundation/SampleFoundation.m
@@ -0,0 +1,13 @@
+//
+//  SampleFoundation.m
+//  SampleFoundation
+//
+//  Created by Harold James D. Tago on 2/25/15.
+//  Copyright (c) 2015 Harold James D. Tago. All rights reserved.
+//
+
+#import "SampleFoundation.h"
+
+@implementation SampleFoundation
+
+@end
